f4ff16d485254a260deb053e9288e0a15e772dd3
lxl518000 <450724951@qq.com>
2018-06-24 13:36:28 +0800

    png

---
+++ webroot/admin.php
@@ -0,0 +1,29 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// 应用入口文件
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+
+
+define('APP_DEBUG',TRUE);
+
+
+$host = $_SERVER['HTTP_HOST'];
+$host = ltrim($host,'www');
+if($host != '6fei.com'){
+	define("CONTROLLER_NAME",'EnIndex');
+}
+
+define('ROOTPATH',dirname(__FILE__).'/');
+
+define('APP_PATH',ROOTPATH.'../Admin/');
+
+require ROOTPATH.'../ThinkPHP/ThinkPHP.php';
